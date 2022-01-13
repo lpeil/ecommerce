eda6097786bfc01ce1b14558a2fd12416dc1d78a
Luan Peil <luan@peil.dev>
2022-01-13 16:04:17 -0300

    Configure eslint and prettier (#1)
    * chore: add eslint to project
    
    * chore: add eslint-import-resolver-typescript package
    
    * chore: config eslint rules and settings
    
    * chore: add prettier to project
    
    * chore: config prettier
    
    * chore: add eslint ignore

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['error', { extensions: ['.ts', '.tsx'] }],
+    'import/extensions': 0,
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+};
