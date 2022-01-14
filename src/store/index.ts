e4e2560c39b8a9a079aa4bfebc37a43de0c8c5c0
Luan Peil <luan@peil.dev>
2022-01-13 22:59:59 -0300

    feat: config redux store

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import rootReducer from './modules/rootReducer';
+
+const enhancer =
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools()
+    : applyMiddleware();
+
+export default createStore(rootReducer, compose(enhancer));
