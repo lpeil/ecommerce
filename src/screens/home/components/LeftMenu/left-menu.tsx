b42e167c3d780a5b797f8df109a4f32c26233552
Luan Peil <luan@peil.dev>
2022-01-17 17:54:51 -0300

    feat(home screen): remove left menu filters chips

--- src/screens/home/components/LeftMenu/left-menu.tsx
+++ src/screens/home/components/LeftMenu/left-menu.tsx
@@ -3,7 +3,6 @@ import { Typography, Box } from '@mui/material';
 
 import PriceSlider from '../PriceSlider';
 import NameSearch from '../NameSearch';
-import FiltersChips from '../FiltersChips';
 
 import '../filters.style.scss';
 
@@ -15,7 +14,6 @@ function HomeLeftMenu({ highestPrice }: HomeLeftMenuProps): JSX.Element {
   return (
     <Box className="left-menu" sx={{ display: { xs: 'none', md: 'block' } }}>
       <Typography variant="h5">Filters</Typography>
-      <FiltersChips highestPrice={highestPrice} />
       <NameSearch />
       <PriceSlider highestPrice={highestPrice} />
     </Box>
